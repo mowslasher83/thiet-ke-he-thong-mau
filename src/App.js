d05b08895305f7ebd856d657ed1efeedc3e7e78b
mowslasher83 <radeswaled@hotmail.com>
2025-08-28 21:00:19 -0500

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from './components/Button';
+
+function App() {
+  return (
+    <div>
+      <h1>Thiết Kế Hệ Thống Mẫu</h1>
+      <Button label="Click me" />
+    </div>
+  );
+}
+
+export default App;
