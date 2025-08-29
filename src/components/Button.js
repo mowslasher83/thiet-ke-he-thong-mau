1c19b6c3e1762f1c095375fa33e4743f0a705097
mowslasher83 <radeswaled@hotmail.com>
2025-08-28 21:00:21 -0500

    feat: initial commit for src/components/Button.js

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css';
+
+const Button = ({ label, onClick }) => {
+  return <button className="custom-button" onClick={onClick}>{label}</button>;
+};
+
+export default Button;
